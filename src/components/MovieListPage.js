ee97eadf9ed4743e3d778a641318e3777ca3366e
fatihyazicioglu <fy134388@gmail.com>
2022-12-28 16:32:51 +0100

    diplayin movies with map()

--- src/components/MovieListPage.js
+++ src/components/MovieListPage.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function MovieListPage() {
   const [searchTerm, setSearchTerm] = useState("");
-
+  const [movies, setMovies] = useState([]);
   const handleSearchTermChange = (e) => {
     setSearchTerm(e.target.value);
   };
@@ -12,14 +12,24 @@ function MovieListPage() {
     fetch(searchURL)
       .then((response) => response.json())
       .then((result) => {
-        console.log(result);
+        setMovies(result.Search);
       });
   };
+
+  const movieItems = movies.map((movie) => {
+    return (
+      <div >
+        <img src={movie.Poster} alt='' />
+        <h3>{movie.Title}</h3>
+      </div>
+    );
+  });
   return (
     <div>
       <h1>Movie List Page</h1>
       Search <input type='text' onChange={handleSearchTermChange} />
       <button onClick={() => fetchMovies(searchTerm)}>Search</button>
+      {movieItems}
     </div>
   );
 }
