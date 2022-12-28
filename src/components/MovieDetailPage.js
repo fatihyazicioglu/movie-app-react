c6fea34cf1b186d88d8ccf9ba3ab790f6bb485dc
fatihyazicioglu <fy134388@gmail.com>
2022-12-28 23:14:13 +0100

    displayin movies in Detail Page

--- src/components/MovieDetailPage.js
+++ src/components/MovieDetailPage.js
@@ -1,22 +1,36 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect , useState} from "react";
+// import {useParams} from "react-router-dom"
+
+
+
 
 function MovieDetailPage(props) {
+  const [movieDetail, setMovieDetail] =useState({})
   useEffect(() => {
+   
     const imdbId = props.match.params.imdbId;
     fetchMovieDetailsById(imdbId);
-  });
+  },[]);
   const fetchMovieDetailsById = (imdbId) => {
     const movieDetailsURL = ` http://www.omdbapi.com/?i=${imdbId}&apikey=b0d2b119&`;
 
     fetch(movieDetailsURL)
       .then((response) => response.json())
       .then((result) => {
-        console.log(result);
-        // setMovies(result.imdbId);
+        // console.log(result);
+        setMovieDetail(result);
       });
   };
-  return <div>MovieDetailPage</div>;
+  return <div>
+    <img src={movieDetail.Poster}/>
+    <h5>{movieDetail.Title}</h5>
+    <p>Released:{movieDetail.Released}</p>
+    <p>Director{movieDetail.Director}</p>
+    <p>Actors:{movieDetail.Actors}</p>
+    <p>Awards:{movieDetail.Awards}</p>
+
+  </div>;
 }
 
 export default MovieDetailPage;
