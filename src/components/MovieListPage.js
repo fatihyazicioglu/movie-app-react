3dc314e32b2c3de95a347f38a014d22cbc815b91
fatihyazicioglu <fy134388@gmail.com>
2022-12-28 23:53:35 +0100

    persisting state,clean old datas, error added

--- src/components/MovieListPage.js
+++ src/components/MovieListPage.js
@@ -1,18 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 function MovieListPage() {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
+  const [noMoviesFound, setNoMoviesFound] = useState(false)
+useEffect(
+  ()=> {
+    let term = localStorage.getItem("searchTerm");
+    if (term) {
+      fetchMovies(term)
+  }},[]
+)
+
+
   const handleSearchTermChange = (e) => {
     setSearchTerm(e.target.value);
   };
+
+  const clearResults =()=>{
+    setMovies([])
+    setSearchTerm('')
+    localStorage.removeItem("searchTerm")
+  }
+
+
   const fetchMovies = (movieName) => {
     const searchURL = ` http://www.omdbapi.com/?s=${movieName}&page=2&apikey=b0d2b119&
   `;
+
+  localStorage.setItem("searchTerm", movieName);
     fetch(searchURL)
       .then((response) => response.json())
       .then((result) => {
-        setMovies(result.Search);
+
+        if(result.Error){
+          setMovies([])
+          setNoMoviesFound(true)
+        }
+        else{
+          setMovies(result.Search);
+          setNoMoviesFound(false)
+
+        }
+        
       });
   };
 
@@ -32,7 +62,10 @@ function MovieListPage() {
       <h1>Movie List Page</h1>
       Search <input type='text' onChange={handleSearchTermChange} />
       <button onClick={() => fetchMovies(searchTerm)}>Search</button>
+      <button onClick={clearResults}>Clear Results</button>
       {movieItems}
+
+      {noMoviesFound ? <h1>No Movies Found</h1> : null}
     </div>
   );
 }
