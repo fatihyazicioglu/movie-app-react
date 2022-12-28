bfb6d27c059a1f25ab631a17858bd0c7c4089b9e
fatihyazicioglu <fy134388@gmail.com>
2022-12-28 16:58:28 +0100

    detail page with react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import MovieListPage from './components/MovieListPage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import MovieListPage from "./components/MovieListPage";
+import MovieDetailPage from "./components/MovieDetailPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MovieListPage />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<MovieListPage/>} />
+        <Route  path='/:imdbId' element={<MovieDetailPage/>} />
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
