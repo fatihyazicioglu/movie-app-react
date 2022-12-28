1e0896be0336fb047c6ea308a04df9ce6d5ce4c5
fatihyazicioglu <fy134388@gmail.com>
2022-12-28 14:48:37 +0100

    fetchin movies

--- src/components/MovieListPage.js
+++ src/components/MovieListPage.js
@@ -1,17 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function MovieListPage() {
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const[searchTerm, setSearchTerm] =useState('')
-
-const handleSearchTermChange = (e)=>{
-setSearchTerm(e.target.value);
-}
-
+  const handleSearchTermChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const fetchMovies = (movieName) => {
+    const searchURL = ` http://www.omdbapi.com/?s=${movieName}&page=2&apikey=b0d2b119&
+  `;
+    fetch(searchURL)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
   return (
     <div>
       <h1>Movie List Page</h1>
-      Search <input type="text" onChange= {handleSearchTermChange} />
+      Search <input type='text' onChange={handleSearchTermChange} />
+      <button onClick={() => fetchMovies(searchTerm)}>Search</button>
     </div>
   );
 }
