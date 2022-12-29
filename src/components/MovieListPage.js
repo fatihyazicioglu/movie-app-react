285b03b42f83961f49253c824f422178d20595c4
fatihyazicioglu <fy134388@gmail.com>
2022-12-29 01:06:26 +0100

    bootstrap adapted

--- src/components/MovieListPage.js
+++ src/components/MovieListPage.js
@@ -3,46 +3,39 @@ import { NavLink } from "react-router-dom";
 function MovieListPage() {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
-  const [noMoviesFound, setNoMoviesFound] = useState(false)
-useEffect(
-  ()=> {
+  const [noMoviesFound, setNoMoviesFound] = useState(false);
+  useEffect(() => {
     let term = localStorage.getItem("searchTerm");
     if (term) {
-      fetchMovies(term)
-  }},[]
-)
-
+      fetchMovies(term);
+    }
+  }, []);
 
   const handleSearchTermChange = (e) => {
     setSearchTerm(e.target.value);
   };
 
-  const clearResults =()=>{
-    setMovies([])
-    setSearchTerm('')
-    localStorage.removeItem("searchTerm")
-  }
-
+  const clearResults = () => {
+    setMovies([]);
+    setSearchTerm("");
+    localStorage.removeItem("searchTerm");
+  };
 
   const fetchMovies = (movieName) => {
     const searchURL = ` http://www.omdbapi.com/?s=${movieName}&page=2&apikey=b0d2b119&
   `;
 
-  localStorage.setItem("searchTerm", movieName);
+    localStorage.setItem("searchTerm", movieName);
     fetch(searchURL)
       .then((response) => response.json())
       .then((result) => {
-
-        if(result.Error){
-          setMovies([])
-          setNoMoviesFound(true)
-        }
-        else{
+        if (result.Error) {
+          setMovies([]);
+          setNoMoviesFound(true);
+        } else {
           setMovies(result.Search);
-          setNoMoviesFound(false)
-
+          setNoMoviesFound(false);
         }
-        
       });
   };
 
@@ -59,17 +52,27 @@ useEffect(
   });
   return (
     <div>
-      <h1>Movie List Page</h1>
-      Search <input type='text' onChange={handleSearchTermChange} />
-      <button onClick={() => fetchMovies(searchTerm)}>Search</button>
-      <button onClick={clearResults}>Clear Results</button>
-      {movieItems}
+      <section class='jumbotron text-center'>
+        <div class='container'>
+          <h1 class='jumbotron-heading'>Movie Search</h1>
+          <p class='lead text-muted'>
+            Start a search see what you will be watching tonight!
+          </p>
 
+          <input type='text' onChange={handleSearchTermChange} placeholder="Search movies, actors or more.." />
+          <p>
+          <button onClick={() => fetchMovies(searchTerm)} class='btn btn-primary my-2 me-2'>Search</button>
+      <button onClick={clearResults} class='btn btn-secondary my-2'>Clear Results</button>
+          </p>
+        </div>
+      </section>
+      
+      
+      
+      {movieItems}
       {noMoviesFound ? <h1>No Movies Found</h1> : null}
     </div>
   );
 }
 
 export default MovieListPage;
-
-
